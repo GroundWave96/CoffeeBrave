3872dfcd02ff7ad6a2d585bfa7f5a95e8cebb182
Vitor Aguena <144974439+vitoraguena17@users.noreply.github.com>
2024-11-20 20:28:46 -0300

    Update index

--- frontend/script/app.js
+++ frontend/script/app.js
@@ -93,48 +93,37 @@ var swiper = new Swiper(".mySwiper", {
     centeredSlides: true,
     spaceBetween: 30,
     pagination: {
-        el: ".swiper-pagination",
         type: "fraction",
     },
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: ".prev",
+        prevEl: ".next",
     },
+    on: {
+        slideChange: function () {
+            updateNavButtons();
+        },
+        reachEnd: function () {
+            updateNavButtons();
+        },
+        reachBeginning: function () {
+            updateNavButtons();
+        }
+    }
 });
 
-var appendNumber = 4;
-var prependNumber = 1;
-document
-    .querySelector(".prepend-2-slides")
-    .addEventListener("click", function (e) {
-        e.preventDefault();
-        swiper.prependSlide([
-            '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
-            '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
-        ]);
-    });
-document
-    .querySelector(".prepend-slide")
-    .addEventListener("click", function (e) {
-        e.preventDefault();
-        swiper.prependSlide(
-            '<div class="swiper-slide">Slide ' + --prependNumber + "</div>"
-        );
-    });
-document
-    .querySelector(".append-slide")
-    .addEventListener("click", function (e) {
-        e.preventDefault();
-        swiper.appendSlide(
-            '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>"
-        );
-    });
-document
-    .querySelector(".append-2-slides")
-    .addEventListener("click", function (e) {
-        e.preventDefault();
-        swiper.appendSlide([
-            '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
-            '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
-        ]);
-    });
+//* BTN OPACITY *\\
+function updateNavButtons() {
+    if (swiper.isEnd) {
+        document.querySelector('.prev').style.opacity = 0.3; 
+    } else {
+        document.querySelector('.prev').style.opacity = 1; 
+    }
+
+    if (swiper.isBeginning) {
+        document.querySelector('.next').style.opacity = 0.3;
+    } else {
+        document.querySelector('.next').style.opacity = 1;
+    }
+}
+updateNavButtons();
